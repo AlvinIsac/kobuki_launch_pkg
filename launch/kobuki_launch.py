c3b41ddb07ef6f173ec2952924983707a621d6b0
Alvin Isac <149538202+AlvinIsac@users.noreply.github.com>
2024-06-10 18:10:59 +0200

    Update kobuki_launch.py

--- launch/kobuki_launch.py
+++ launch/kobuki_launch.py
@@ -10,10 +10,10 @@ def generate_launch_description():
             output='screen',
             parameters=[
                 {'device_port': '/dev/ttyUSB0'},
-                {'base_frame': 'base'},
+                
             ],
             remappings=[
-                ('commands/velocity', 'cmd_vel'),
+                ('commands/velocity', 'cmd/vel'),
                 ('commands/led1', 'cmd/led1'),
                 ('commands/led2', 'cmd/led2'),
                 ('commands/digital_output', 'cmd/digital_output'),
@@ -22,6 +22,23 @@ def generate_launch_description():
                 ('commands/reset_odometry', 'cmd/reset_odometry'),
                 ('commands/motor_power', 'cmd/motor_power'),
                 ('commands/controller_info', 'cmd/controller_info'),
+                ('sensors/core', 'sensors/core'),
+                ('sensors/dock_ir', 'sensors/dock_ir'),
+                ('sensors/battery_state', 'sensors/battery_state'),
+                ('sensors/imu_data', 'sensors/imu_data'),
+                ('sensors/imu_data_raw', 'sensors/imu_data_raw'),
+                ('debug/raw_data_command', 'debug/raw_data_command'),
+                ('debug/raw_data_stream', 'debug/raw_data_stream'),
+                ('debug/raw_control_command', 'debug/raw_control_command'),
+                ('odom', 'odom'),
+                ('joint_states', 'joint_states'),
+                ('events/button', 'events/button'),
+                ('events/bumper', 'events/bumper'),
+                ('events/cliff', 'events/cliff'),
+                ('events/wheel_drop', 'events/wheel_drop'),
+                ('events/power_system', 'events/power_system'),
+                ('events/digital_input', 'events/digital_input'),
+                ('events/robot_state', 'events/robot_state'),
             ]
         )
     ])
